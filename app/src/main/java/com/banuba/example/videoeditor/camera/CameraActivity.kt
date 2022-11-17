60800ec33317f2149e6a91ffd5722e02c0f94bda
ve-sdk-android <73172228+ve-sdk-android@users.noreply.github.com>
2022-11-17 14:58:47 +0300

    Implement preparing AR effects

--- app/src/main/java/com/banuba/example/videoeditor/camera/CameraActivity.kt
+++ app/src/main/java/com/banuba/example/videoeditor/camera/CameraActivity.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
@@ -103,6 +104,7 @@ class CameraActivity : AppCompatActivity() {
     private val timeBasedFileNameFormat = SimpleDateFormat("yyyy.MM.dd_HH.mm.ss", Locale.ENGLISH)
 
     private var banubaSdkManager: BanubaSdkManager? = null
+    private var effectsHelper = BanubaEffectHelper()
 
     private var isBeautyApplied = false
     private var isMaskApplied = false
@@ -213,18 +215,22 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun applyEffect(name: String) {
-        val effectManager = banubaSdkManager?.effectManager ?: return
-        val maskUrl = Uri.parse(BanubaSdkManager.getResourcesBase())
-            .buildUpon()
-            .appendPath("effects")
-            .appendPath(name)
-            .build()
-            .toString()
-        effectManager.loadAsync(maskUrl)
+        val manager = banubaSdkManager?.effectManager
+        if (manager == null) {
+            Log.w(
+                "CameraActivity",
+                "Cannot apply effect: Banuba Face AR Effect Player is not initialized"
+            )
+        } else {
+            CoroutineScope(Dispatchers.IO).launch {
+                val effect = effectsHelper.prepareEffect(assets, name)
+                manager.loadAsync(effect.uri.toString())
+            }
+        }
     }
 
     private fun cancelEffect() {
-        banubaSdkManager?.effectManager?.loadAsync("maskUrl") ?: return
+        banubaSdkManager?.effectManager?.loadAsync("") ?: return
     }
 
     private fun prepareFaceAR() {
