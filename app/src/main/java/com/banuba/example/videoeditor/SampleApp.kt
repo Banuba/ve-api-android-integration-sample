d21136d12686d9ae0c7d7e23589a1fc67add2f0b
ve-sdk-android <sdk-ve-android@banuba.com>
2023-01-10 18:06:39 +0300

    Update migration to 1.26.3

--- app/src/main/java/com/banuba/example/videoeditor/SampleApp.kt
+++ app/src/main/java/com/banuba/example/videoeditor/SampleApp.kt
@@ -1,6 +1,7 @@
 package com.banuba.example.videoeditor
 
 import android.app.Application
+import android.util.Log
 import com.banuba.sdk.token.storage.license.BanubaVideoEditor
 
 class SampleApp : Application() {
@@ -9,6 +10,18 @@ class SampleApp : Application() {
 
         BanubaVideoEditorSDK().initialize(this@SampleApp)
 
-        BanubaVideoEditor.initialize(getString(R.string.banuba_token))
+        val videoEditorSDK = BanubaVideoEditor.initialize(getString(R.string.banuba_token))
+
+        if (videoEditorSDK == null) {
+            Log.e("BanubaVideoEditor", "BanubaVideoEditor initialization error")
+        } else {
+            videoEditorSDK.getLicenseState { isValid ->
+                if (isValid) {
+                    Log.d("BanubaVideoEditor", "BanubaVideoEditor token is valid")
+                } else {
+                    Log.d("BanubaVideoEditor", "BanubaVideoEditor token is not valid")
+                }
+            }
+        }
     }
 }
